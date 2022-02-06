cb642243141b87be62ae3c479d2ee4db6aff7e1a
rupertviglione <71977082+rupertviglione@users.noreply.github.com>
2022-02-06 12:57:48 +0000

    puppeteer

---
+++ assets/index.js
@@ -0,0 +1,16 @@
+const projectname = "seiva"
+const urlToTest = [
+    "https://www.rupertviglione.github.io",
+    "https://www.rupertviglione.github.io/agenda.html",
+    "https://www.rupertviglione.github.io/atelier.html",
+    "https://www.rupertviglione.github.io/bazar.html",
+    "https://www.rupertviglione.github.io/const.html",
+    "https://www.rupertviglione.github.io/drogaria.html",
+    "https://www.rupertviglione.github.io/ecodesign.html",
+    "https://www.rupertviglione.github.io/educacao.html",
+    "https://www.rupertviglione.github.io/equipa.html",
+    "https://www.rupertviglione.github.io/index.html",
+    "https://www.rupertviglione.github.io/missao.html",
+    "https://www.rupertviglione.github.io/quemsomos.html",
+    "https://www.rupertviglione.github.io/yoga.html",
+]
